5631c69ba7f0068870204403db90279dac7e7049
Cameron Raymond <15cjkr@queensu.ca>
2019-02-11 19:47:11 -0500

    test

--- nqueens.py
+++ nqueens.py
@@ -1,30 +1,37 @@
 import random
-"""The n queens puzzle"""
+import time
 
+"""The n queens puzzle"""
 
 class NQueens:
     """Generate all valid solutions for the n queens puzzle"""
-
     def __init__(self, size):
         # Store the puzzle (problem) size and the number of valid solutions
+        print("Size:  "+str(size)+" initializing positions")
         self.totalConflicts = 0
         self.size = size
         self.positions = self.initializePositions(size)
-        self.show_full_board(self.positions)
         self.solve()
-        self.show_full_board(self.positions)
+        print("Solved")
 
     def solve(self):
+        i = 1
         while self.totalConflicts > 0:
+            start = time.time()
             for column in range(len(self.positions)):
                 if self.totalConflicts <= 0:
                     break
                 row = self.positions[column]
                 numConflicts = self.numConflicts(row,column,self.positions)
                 rowToPut, minConflicts = self.minConflicts(column,self.positions)
-                # print("Moved queen in column "+str(column)+" from row "+str(row) + " to row " + str(rowToPut)+". Num conflicts: "+str(self.totalConflicts))
                 self.totalConflicts += (minConflicts-numConflicts)
                 self.positions[column] = rowToPut
+            end = time.time()
+            print("Pass "+str(i)+" - duration: "+str(end - start)+", total number of conflicts " + str(self.totalConflicts))
+            # self.show_full_board(self.positions)
+
+            i+=1
+        self.show_full_board(self.positions)
     
     def initializePositions(self,size):
         board = [None] * size
@@ -68,7 +75,6 @@ class NQueens:
                     total += 1
         return total
 
-
     def show_full_board(self, positions):
         """Show the full NxN board"""
         for row in range(self.size):
@@ -82,19 +88,16 @@ class NQueens:
         print("Total conflicts: "+str(self.totalConflicts))
         print("\n")
 
-
 def readText(fname):
     with open(fname) as f:
         content = f.readlines()
     content = [int(x.strip()) for x in content]
     return content
 
-
-
-
 if __name__ == '__main__':
     sizes = readText('./nqueens.txt')
-
-    for size in sizes:
-        queen = NQueens(size)
+    start = time.time()
+    queen = NQueens(300)
+    end = time.time()
+    print("Time elapsed: "+str(end - start))
     
